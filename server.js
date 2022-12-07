1a92d60f99d1ea8e92c8af212ddfc1b9dc542723
Bharath Jasti <43598150+bharathjasti98@users.noreply.github.com>
2022-12-06 22:16:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const url = require('url');
 const path = require('path');
 const fs = require('fs');
 const mongourl = "mongodb+srv://Jastibharath:1426@cluster0.9dj9h9i.mongodb.net/?retryWrites=true&w=majority";
-//mongodb+srv://Jastibharath:<password>@cluster0.9dj9h9i.mongodb.net/?retryWrites=true&w=majority
-
 const port = process.env.port || 8090;
-const requestListener = async function (req, res) {
+const request = async function (req, res) {
     if (req.url === '/about') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
@@ -22,36 +20,30 @@ const requestListener = async function (req, res) {
             res.end(data);
         })
     } else {
-
         res.writeHead(200, { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" });
         res.write(JSON.stringify(await dbClient()));
         res.end();
     }
 };
-
-const server = http.createServer(requestListener);
-server.listen(port,() => {
-    console.log(`Server is running on http://${port}`);
-});
-
 async function dbClient() {
     const client = new MongoClient(mongourl);
     const list = await Granite_Usa(client);
     return list;
 }
-
-
 async function Granite_Usa(client) {
     var Granite_Details = null;
     try {
         client.connect();
-
         Granite_Details = await client.db("Granite").collection("Granite_details").find().toArray();
     } catch (e) {
         console.error(e);
     } finally {
         await client.close();
     }
-
     return Granite_Details;
 }
+const server = http.createServer(request);
+server.listen(port, () => {
+    console.log(`Server on http://${port}`);
+});
+
